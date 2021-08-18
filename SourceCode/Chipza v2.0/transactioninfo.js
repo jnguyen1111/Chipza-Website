35d2d7aada02d533b1501ceb2396503cbbc99c60
John Nguyen <78591393+jnguyen1111@users.noreply.github.com>
2021-08-18 01:42:15 -0500

    v2.0

--- SourceCode/Chipza v2.0/transactioninfo.js
+++ SourceCode/Chipza v2.0/transactioninfo.js
@@ -1,64 +1,78 @@
 
-// function checkPhoneNumber(test8,digits){
-//      if(test8.length != 10){
-//          return(false);
-//      }
-//      for(let i = 0; i < test8.length; i++){
-//         if(test8[i] in digits){
-//              //pass do nothing
-//          }
-//          else{
-//              return(false);
-//          }
-//     }
-//     return true;
-// }
-
-// function checkZipCode(test4,digits){
-//      if(test4.length != 5){
-//          return(false);
-//      }
-//      for(let i = 0; i < test4.length; i++){
-//         if(test4[i] in digits){
-//              //pass do nothing
-//          }
-//          else{
-//              return(false);
-//          }
-//     }
-//     return true;
-// }
-
-// function checkShipType(test9){
-//     if(!test9){
-//         return(false);
-//     }
-//     else{
-//         test9 = test9.value;
-//         return(true);
-//     }
-// }
+let stateTax = null;
+let productCost = null;
+let productQuantity = null;
+let shippingCost = null;
 
 function testFunction(){
 
-    const digits = ["0","1","2","3","4","5","6","7","8","9"];
+    let firstName = document.getElementById("first-name").value;
+    let lastName = document.getElementById("last-name").value;
+    let shippingAddress = document.getElementById("shipping-address").value;
+    let zipCode = document.getElementById("zip-code").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    let email = document.getElementById("email").value;
+    let phoneNumber = document.getElementById("phone-number").value;
+    let shippingTypes = document.querySelector('input[name="shippingtypes"]:checked');
+    let product = document.getElementById("item").value;
+    let quantity = document.getElementById("quantity").value;
 
-    let test1 = document.getElementById("first-name").value;
-    let test2 = document.getElementById("last-name").value;
-    let test3 = document.getElementById("shipping-address").value;
-    let test4 = document.getElementById("zip-code").value;
-    let test5 = document.getElementById("city").value;
-    let test6 = document.getElementById("state").value;
-    let test7 = document.getElementById("email").value;
-    let test8 = document.getElementById("phone-number").value;
-    let test9 = document.querySelector('input[name="shippingtypes"]:checked');
-    let test10 = document.getElementById("item").value;
-    let test11 = document.getElementById("quantity").value;
+    //To dofind a way to validate values
 
-    alert("testing 123 values");
+    return false;
 
-    // if( !(test1 == "")){ 
-    //     alert("Have a good day " + test1 +" thank you for shopping at chipza!");
-    // }
+}
+
+function updateStateTax(){
+    stateTax= document.querySelector(("#"+document.getElementById("state").value)).getAttribute("data-tax");
+    updateTotalCost();
+    return false;
+}
+
+function updateShipPrice(){
+    shippingCost = document.querySelector('input[name="shippingtypes"]:checked').value;
+    document.getElementById("shipping-cost").innerHTML = ("shipping: $" + shippingCost);
+    updateTotalCost();
+    return false;
+}
+
+function updateProductPrice(){
+    productName = document.getElementById("item").value;
+    if(productName == ""){
+        productCost = 0.00;
+        document.getElementById("product-cost").innerHTML = ("subtotal: $" + productCost);
+        document.getElementById("model-number").innerHTML = ("Item #: ");
+    }
+    else{
+        productCost = document.querySelector(("#"+productName)).getAttribute("data-price")
+        document.getElementById("product-cost").innerHTML = ("subtotal: $" + productCost);
+        document.getElementById("model-number").innerHTML = ("Item #: " + document.querySelector(("#"+ productName)).getAttribute("data-model"));
+    }
+    updateTotalCost();
+
+    return false;
+}
+
+function updateQuantity(){
+    if(productCost == "" || productCost == null){
+        alert("Please select the product you wish to buy.");
+    }
+    else{
+        productQuantity = document.getElementById("quantity").value;
+        document.getElementById("product-cost").innerHTML = ("subtotal: $" + (Number(productCost) * Number(productQuantity)));
+    }
+    updateTotalCost();
+    return false;
+}
+
+function updateTotalCost(){
+    if( (stateTax != null && stateTax != "") && (productCost != null && productCost != "") 
+    && (productQuantity != null && productQuantity != "") && (shippingCost != null && shippingCost != "")  ){
+        let subtotal =   (Number(productCost) * Number(productQuantity))  + Number(shippingCost) ; 
+        let tax = subtotal * stateTax;
+        document.getElementById("tax-cost").innerHTML = ("tax: $" + tax.toFixed(2) );
+        document.getElementById("total-cost").innerHTML = ( ("total: $" + (Number(subtotal) + Number(tax)))); 
+    }
 }
 
